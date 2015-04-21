1aa9849cb220f0d0bbeed8a62c54296dbd2a15e6
Sammy Lin <sammylintw@gmail.com>
2015-04-21 15:36:38 +0800

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 
-# Function that tests if a command exists
-command_exists() {
-  command -v "$1" &> /dev/null ;
-}
-
-# Install chef-solo command if it doesn't exist (using omnibus installer)
-if command_exists chef-solo; then
-  echo "Chef already installed"
-else
-  echo "Installing chef to: /opt/chef/"
-  curl -L https://www.opscode.com/chef/install.sh | bash
-  echo ""
-fi
-
 # Download osx-cookbook
 if [ ! -d "~/osx-cookbook/" ]; then
   echo "Download osx-cookbook"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Function that tests if a command exists
+command_exists() {
+  command -v "$1" &> /dev/null ;
+}
+
+
 # Add /var/chef/ if it doesn't exist
 if [ ! -d "/var/chef/" ]; then
   echo "Creating /var/chef/"
@@ -14,4 +20,13 @@ if [ ! -d "/usr/local/" ]; then
   sudo mkdir -p /usr/local && sudo chown -R `whoami`:staff /usr/local
 else
   echo "Directory /usr/local already exists"
+fi
+
+# Install chef-solo command if it doesn't exist (using omnibus installer)
+if command_exists chef-solo; then
+  echo "Chef already installed"
+else
+  echo "Installing chef to: /opt/chef/"
+  curl -L https://www.opscode.com/chef/install.sh | sudo bash
+  echo ""
 fi
